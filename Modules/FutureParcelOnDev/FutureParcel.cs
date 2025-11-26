766bcc1bafd494bc0055565b50af5b3cf0db0b0c
DavidY3Wv <a2024110134@estudiantes.upsa.edu.bo>
2025-11-26 15:00:41 -0400

    feat:Permitir que los residentes pueda recoger sus paquetes sin tener que realizar tantos procedimientos

---
+++ Modules/FutureParcelOnDev/FutureParcel.cs
@@ -0,0 +1,14 @@
+/*namespace HabiTechs.Modules.FuturePacketingOnDev;
+
+public class FuturePacketing
+{
+    
+}
+En investigación en como podría implementar el sistema de paquetería en la app ,mediante registro
+QR donde el QR deberá de llenar con datos de residente X y un código a la cual se le enviará mediante
+app para poder canjear su paquete en el almacen.
+
+Esta función está planeado para implementar en un futuro como algo secundario 
+no entra como función principal de la app.
+*/
+
